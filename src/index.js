4f89b43185a12475129262eb70976500f6c0a52f
ShererStress <ryanswhitehill@gmail.com>
2019-06-05 09:53:59 -0600

    Disabled serviceworker, hoping I dont need it later

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './normalize.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// registerServiceWorker();
+// serviceWorker.unregister();
