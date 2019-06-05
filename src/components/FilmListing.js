222ae54d9e30b20bf1c6a106253db4ff9e6109cf
ShererStress <ryanswhitehill@gmail.com>
2019-06-05 10:54:05 -0600

    Refactored FilmListing component to show number of films. Setting up click functionality.

--- src/components/FilmListing.js
+++ src/components/FilmListing.js
@@ -2,16 +2,43 @@ import React, { Component } from 'react';
 import FilmRow from './FilmRow.js';
 
 class FilmListing extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleFilterClick = this.handleFilterClick.bind(this);
+
+  }
+
+  handleFilterClick(filter) {
+    console.log(`setting message to ${filter}`);
+  }
+
   render() {
+
+    let allFilms = this.props.filmData.map(function(film, index){
+      return (
+        <FilmRow singleFilm={film} key={film.id}/>
+      )
+    });
+
     return (
-      <div className="film-list">
-        <h1 className="section-title">FILMS</h1>
-        {this.props.filmData.map(function(film, index){
-          return (
-            <FilmRow singleFilm={film} key={film.id}/>
-          )
-        })}
+    <div className="film-list">
+      <h1 className="section-title">FILMS</h1>
+      <div className="film-list-filters">
+        <div className="film-list-filter">
+          ALL
+          <span className="section-count">{this.props.filmData.length}</span>
+        </div>
+        <div className="film-list-filter">
+          FAVES
+          <span className="section-count">0</span>
+        </div>
       </div>
+      <h3 onClick={this.handleFilterClick.bind(this, "AAA")}> FIUGFGISF </h3>
+
+      {allFilms}
+    </div>
     );
   }
 }
